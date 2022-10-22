04d014800e7ddaaf58276be8ab1f4a385148f4b7
XtarNodde <onyekaejimns@gmail.com>
2022-10-22 14:29:22 +0100

    dashboard

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class DashBoard extends StatefulWidget {
@@ -8,11 +10,121 @@ class DashBoard extends StatefulWidget {
 }
 
 class _DashBoardState extends State<DashBoard> {
+  bool fav = false;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.cyan,
-      body: Center(child: Text("DashBoard")),
+    print(MediaQuery.of(context).size.height);
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white70,
+        body: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          width: double.infinity,
+          child: Column(
+            children: [
+              SizedBox(
+                  height: 100,
+                  width: double.infinity,
+                  child: Card(
+                    elevation: 5,
+                    shadowColor: Colors.amber,
+                    color: Colors.white,
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          CircleAvatar(
+                            radius: 50,
+                            backgroundImage: AssetImage("asset/lucas.jpg"),
+                          ),
+                          Text(
+                            "Adamu",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ]),
+                  )),
+              Expanded(
+                child: GridView.builder(
+                  // physics: NeverScrollableScrollPhysics(),
+
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 20,
+                      crossAxisSpacing: 20),
+                  itemBuilder: (context, index) => Material(
+                    borderRadius: BorderRadius.circular(20),
+                    shadowColor: Colors.amber,
+                    elevation: 3,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: GridTile(
+                        header: Container(
+                          height: 40,
+                          color: Colors.black.withOpacity(0.4),
+                          child: Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                IconButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        fav = !fav;
+                                      });
+                                    },
+                                    icon: fav == true
+                                        ? Icon(
+                                            Icons.favorite,
+                                            color: Colors.red,
+                                          )
+                                        : Icon(
+                                            Icons.favorite_border_rounded,
+                                            color: Colors.white,
+                                          )),
+                                SizedBox(
+                                  width: 20,
+                                )
+                              ]),
+                        ),
+                        footer: Container(
+                          height: 40,
+                          color: Colors.black.withOpacity(0.7),
+                          child: Column(children: [
+                            Text(
+                              "adad",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 20.0,right: 20),
+                              child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+
+                                  Text(
+                                    "\$1200",
+                                    style: TextStyle(color: Colors.white60,decoration: TextDecoration.lineThrough,),
+
+                                  ),   Text(
+                                    "\$1200",
+                                    style: TextStyle(color: Colors.green),
+                                  )
+                                ],
+                              ),
+                            )
+                          ]),
+                        ),
+                        child: Image.asset(
+                          "asset/lucas.jpg",
+                          fit: BoxFit.fill,
+                          height: double.infinity,
+                          width: double.infinity,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
